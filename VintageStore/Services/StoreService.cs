946501972fd37ab2127de8902bb3f1b9c61aeb0a
User <User@LAB3-3>
2023-12-20 14:53:07 +0200

    update

--- VintageStore/Services/StoreService.cs
+++ VintageStore/Services/StoreService.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Java.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using VintageStore.Models;
 
 namespace VintageStore.Services
 {
@@ -27,5 +32,45 @@ namespace VintageStore.Services
         {
             throw new NotImplementedException();
         }
+
+        public async Task<LoginDTO> LoginAsync(string UserName, string Password)
+        {
+            try
+            {
+                var user = new LoginDTO() { User = UserName, Password = Password };
+                var jsonContent = JsonSerializer.Serialize(user, _serializerOptions);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync($"{URL}LoginPage", content);
+
+                switch (response.StatusCode)
+                {
+                    case (HttpStatusCode.OK):
+                        {
+                            jsonContent = await response.Content.ReadAsStringAsync();
+                            User u = JsonSerializer.Deserialize<User>(jsonContent, _serializerOptions);
+                            await Task.Delay(2000);
+                            return new LoginDTO() { Success = true, Message = string.Empty, Password = u.UserPswd, User = u.UserName };
+
+                        }
+                    case (HttpStatusCode.Unauthorized):
+                        {
+                            return new LoginDTO() { Success = false, User = null, Password = null, Message = Models.Messages.INVALID_LOGIN };
+
+
+                        }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return new LoginDTO() { Success = false, User = null, Password = null, Message = Models.Messages.INVALID_LOGIN };
+
+
+        }
+
+
     }
 }
