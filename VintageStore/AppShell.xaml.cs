fdedb59a5b27fc78f82a378514fda48f2bf1055a
User <User@LAB3-3>
2024-04-11 13:11:55 +0300

    update

--- VintageStore/AppShell.xaml.cs
+++ VintageStore/AppShell.xaml.cs
@@ -2,6 +2,7 @@
 using VintageStore.Views;
 
 using VintageStore.Services;
+using VintageStore.Models;
 
 namespace VintageStore
 {
@@ -18,15 +19,16 @@ namespace VintageStore
             Routing.RegisterRoute("EnvironmentalPage", typeof(EnvironmentalPage));
             Routing.RegisterRoute("LogOutPage", typeof(LogOutPage));
             
+            LogOutCommand = new Command<StoreService>(LogOut);
         }
 
         StoreService service;
         public ICommand LogOutCommand { get; private set; }
-        private async Task LogOut (StoreService _service)
+        private async void LogOut (StoreService _service)
         {
             service=_service;
            _service.SetCurrentUser(null);
-
+            await Shell.Current.GoToAsync("///MainPage");
 
 
         }
